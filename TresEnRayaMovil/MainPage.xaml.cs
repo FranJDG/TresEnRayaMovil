753cb65ee2e23a7f2e907ed348d41ba2b9928911
Fran Diaz <frandiazguitar@gmail.com>
2023-09-17 13:04:42 +0200

    - Con mensajes en pantalla temporales

--- TresEnRayaMovil/MainPage.xaml.cs
+++ TresEnRayaMovil/MainPage.xaml.cs
@@ -32,13 +32,13 @@
         }
 
 
-        private void Button_Click(object sender, EventArgs e)
+        private async void Button_Click(object sender, EventArgs e)
         {
             ImageButton boton = (ImageButton)sender;
 
             if (boton.ClassId != null)
             {
-                DisplayAlert("", "Debes pulsar en una casilla vacía.", "Ok");
+                await MensajeCasillaVacia();
 
             }
             else
@@ -47,14 +47,14 @@
                 {
                     boton.Source = "xficha.gif";
                     boton.ClassId = "x";
-                    Ganador();
+                    await Ganador();
                     turno.Text = "Jugador O";
                 }
                 else
                 {
                     boton.Source = "oficha.gif";
                     boton.ClassId = "o";
-                    Ganador();
+                    await Ganador();
                     turno.Text = "Jugador X";
                 }
 
@@ -63,67 +63,96 @@
         }
 
 
-        private void Ganador()
+        private async Task Ganador()
         {
             if (btn1.ClassId != null && btn2.ClassId != null && btn3.ClassId != null && btn1.ClassId == btn2.ClassId && btn2.ClassId == btn3.ClassId)
             {
-                MensajeGanador();
+                await MensajeGanador();
                 Resultado();
                 ReiniciarJuego();
             }
             else if (btn4.ClassId != null && btn5.ClassId != null && btn6.ClassId != null && btn4.ClassId == btn5.ClassId && btn5.ClassId == btn6.ClassId)
             {
-                MensajeGanador();
+                await MensajeGanador();
                 Resultado();
                 ReiniciarJuego();
             }
             else if (btn7.ClassId != null && btn8.ClassId != null && btn9.ClassId != null && btn7.ClassId == btn8.ClassId && btn8.ClassId == btn9.ClassId)
             {
-                MensajeGanador();
+                await MensajeGanador();
                 Resultado();
                 ReiniciarJuego();
             }
             else if (btn1.ClassId != null && btn4.ClassId != null && btn7.ClassId != null && btn1.ClassId == btn4.ClassId && btn4.ClassId == btn7.ClassId)
             {
-                MensajeGanador();
+                await MensajeGanador();
                 Resultado();
                 ReiniciarJuego();
             }
             else if (btn2.ClassId != null && btn5.ClassId != null && btn8.ClassId != null && btn2.ClassId == btn5.ClassId && btn5.ClassId == btn8.ClassId)
             {
-                MensajeGanador();
+                await MensajeGanador();
                 Resultado();
                 ReiniciarJuego();
             }
             else if (btn3.ClassId != null && btn6.ClassId != null && btn9.ClassId != null && btn3.ClassId == btn6.ClassId && btn6.ClassId == btn9.ClassId)
             {
-                MensajeGanador();
+                await MensajeGanador();
                 Resultado();
                 ReiniciarJuego();
             }
             else if (btn1.ClassId != null && btn5.ClassId != null && btn9.ClassId != null && btn1.ClassId == btn5.ClassId && btn5.ClassId == btn9.ClassId)
             {
-                MensajeGanador();
+                await MensajeGanador();
                 Resultado();
                 ReiniciarJuego();
             }
             else if (btn3.ClassId != null && btn5.ClassId != null && btn7.ClassId != null && btn3.ClassId == btn5.ClassId && btn5.ClassId == btn7.ClassId)
             {
-                MensajeGanador();
+                await MensajeGanador();
                 Resultado();
                 ReiniciarJuego();
             }
             else if (btn1.ClassId != null && btn2.ClassId != null && btn3.ClassId != null && btn4.ClassId != null && btn5.ClassId != null &&
                 btn6.ClassId != null && btn7.ClassId != null && btn8.ClassId != null && btn9.ClassId != null)
             {
-                DisplayAlert("", "Empate!!", "Ok");
+                await MensajeEmpate();
                 ReiniciarJuego();
             }
         }
 
-        private void MensajeGanador()
+        private async Task MensajeGanador()
         {
-            DisplayAlert("", "Ha ganado el " + turno.Text, "Ok");
+            winner.Text = turno.Text.ToUpper();
+            winnerFrame.IsVisible = true;
+
+            await Task.Delay(3000);
+
+            winnerFrame.IsVisible = false;
+        }
+
+        private async Task MensajeEmpate()
+        {
+            winner.Text = "EMPATE!!";
+            ganador.IsVisible = false;
+            winnerFrame.IsVisible = true;
+
+            await Task.Delay(3000);
+
+            winnerFrame.IsVisible = false;
+            ganador.IsVisible = true;
+        }
+
+        private async Task MensajeCasillaVacia()
+        {
+            winner.Text = "Debes pulsar en una casilla vacía";
+            ganador.IsVisible = false;
+            winnerFrame.IsVisible = true;
+
+            await Task.Delay(3000);
+
+            winnerFrame.IsVisible = false;
+            ganador.IsVisible = true;
         }
 
         private void ReiniciarJuego()
